4170b47ac9983a539ebdf3cd09f12cdf52dd5ee1
mouhebDesigner <mouhebabderrahim@gmail.com>
2021-03-20 23:03:27 +0100

    Marwa laravel project

---
+++ database/migrations/2021_03_17_212031_create_enseignants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnseignantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enseignants', function (Blueprint $table) {
+            $table->id();
+            $table->string('specialite');
+            $table->string('type');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enseignants');
+    }
+}
